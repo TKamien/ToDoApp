4df5e07718530271c6488e114a45ed3be1a4c722
TKamien <46108156+TKamien@users.noreply.github.com>
2019-07-24 23:12:05 +0200

    pierwszy
    początek pracy, zbudowane drzewo

---
+++ todoapp/src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './App.scss';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TODO APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
+
