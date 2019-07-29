9ade5c4139236449903b4d76fd71f60cd9047e95
TKamien <46108156+TKamien@users.noreply.github.com>
2019-07-29 22:01:28 +0200

    wersja trzecia
    dodawanie tasków działa, do skończenie jeszcze stylowanie

--- todoapp/src/components/App.js
+++ todoapp/src/components/App.js
@@ -5,12 +5,66 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    tasks: [
+      {
+        id: 0,
+        text: 'skończyć apkę ToDo',
+        date: '2019-08-01',
+        important: true,
+        active: true,
+        finishDate: null
+      },
+    ]
+  }
+
+  deleteTask = (id) => {
+
+    let tasks = [...this.state.tasks];
+    tasks = tasks.filter(task => task.id !== id)
+    this.setState({
+      tasks
+    })
+  }
+
+  changeTaskStatus = (id) => {
+    const tasks = Array.from(this.state.tasks);
+    tasks.forEach(task => {
+      if (task.id === id) {
+        task.active = false;
+        task.finishDate = new Date().getTime()
+      }
+    })
+    this.setState({
+      tasks
+    })
+  }
+
+  addTask = (text, date, important) => {
+    const task = {
+      id: this.counter,
+      text, 
+      date,
+      important, 
+      active: true,
+      finishDate: null
+    }
+    this.counter++
+
+    this.setState(prevState => ({
+      tasks: [...prevState.tasks, task]
+    }))
+
+
+    return true
+  }
+
   render() {
     return (
       <div className="App">
         <h1>ToDo APP</h1>
-        <NewTask/>
-        <ListOfTasks/>
+        <NewTask add={this.addTask}/>
+        <ListOfTasks tasks={this.state.tasks} delete={this.deleteTask} change={this.changeTaskStatus} />
       
       </div>
     );
--- todoapp/src/components/ListOfTasks.js
+++ todoapp/src/components/ListOfTasks.js
@@ -1,5 +1,55 @@
 import React from 'react';
 import Tasks from './Tasks'
 
+const ListOfTasks = (props) => {
+    const active = props.tasks.filter(task => task.active);
+  const done = props.tasks.filter(task => !task.active);
+
+
+  if (done.length >= 2) {
+    done.sort((a, b) => {
+      if (a.finishDate < b.finishDate) {
+        return 1
+      }
+      if (a.finishDate > b.finishDate) {
+        return -1
+      }
+      return 0
+    })
+  }
+  if (active.length >= 2) {
+    active.sort((a, b) => {
+
+      a = a.text.toLowerCase();
+      b = b.text.toLowerCase();
+
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0
+    })
+  }
+
+    const activeTasks = active.map(task => <Tasks key={task.id} task={task} delete={props.delete} change={props.change} />)
+    const doneTasks = done.map(task => <Tasks key={task.id} task={task} delete={props.delete} change={props.change} />)
+    
+    return(
+        <>
+      <div className="active">
+        <h1>Co mam zrobić</h1>
+        {activeTasks.length > 0 ? activeTasks : <p>Brak zadań do wykonania</p>}
+      </div>
+
+      <hr />
+ 
+      <div className="done">
+        <h3>Co już zrobiłem <em>({done.length})</em></h3>
+        {done.length > 5 && <span style={{ fontSize: 10 }}>wyświetlonych jest jedynie 5 ostatnich zadań</span>}
+        {doneTasks.slice(0, 5)}
+
+      </div>
+    </>
+    )
+}
+
 
 export default ListOfTasks;
