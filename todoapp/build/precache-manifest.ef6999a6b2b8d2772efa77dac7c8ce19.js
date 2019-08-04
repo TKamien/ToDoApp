a038722c3cf85c65a53e1f31ca896bb5bdb97719
TKamien <46108156+TKamien@users.noreply.github.com>
2019-08-04 13:11:00 +0200

    do publikacji

---
+++ todoapp/build/precache-manifest.ef6999a6b2b8d2772efa77dac7c8ce19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e469c372dea228c339cc65bd8ae5a655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cd42232fb88a030f9dc",
+    "url": "/static/css/main.d849c382.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "3cd42232fb88a030f9dc",
+    "url": "/static/js/main.418dfada.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
